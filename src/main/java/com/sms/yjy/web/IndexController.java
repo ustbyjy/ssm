9e203472f262b85e08a52121e05dab4caa0f4a29
ustbyjy <ustbyjy@163.com>
2017-06-10 21:02:42 +0800

    update

---
+++ src/main/java/com/sms/yjy/web/IndexController.java
@@ -0,0 +1,25 @@
+package com.sms.yjy.web;
+
+import com.mzlion.easyokhttp.HttpClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Administrator
+ * Date: 2017-01-02
+ * Time: 15:41
+ */
+@RestController
+public class IndexController {
+
+    @GetMapping("/index")
+    public String index() {
+        String cityInfo = HttpClient
+                .get("http://www.weather.com.cn/data/cityinfo/101010100.html")
+                .execute()
+                .asString();
+        return cityInfo;
+    }
+}
