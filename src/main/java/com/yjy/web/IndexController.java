7cabc8c177f88dc5a21bca6bac95cf640b579a4c
yanjingyang <yanjingyang@zulong.com>
2017-09-13 12:06:42 +0800

    update

--- src/main/java/com/yjy/web/IndexController.java
+++ src/main/java/com/yjy/web/IndexController.java
@@ -17,20 +17,12 @@ public class IndexController {
 
     @GetMapping("index")
     public String index() {
+
         String cityInfo = HttpClient
                 .get("http://www.weather.com.cn/data/cityinfo/101010100.html")
                 .execute()
                 .asString();
+
         return cityInfo;
-    }
-
-    @PostMapping("server-list")
-    public String serverList(Long gameid, String project_alias, String filename, String contents) {
-        System.out.println("gamid=" + gameid);
-        System.out.println("project_alias=" + project_alias);
-        System.out.println("filename=" + filename);
-        System.out.println("contents=" + contents);
-
-        return "success";
     }
 }
